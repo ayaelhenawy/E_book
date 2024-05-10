6a2ad887719714819e6ea312d9e832054f32774c
aya11aly <aya.123.aly.5@gmail.com>
2024-05-11 02:11:43 +0300

    add localstorage to search

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,33 @@ if(loginClose){
     User.textContent = userNameStore;
 
 
-    
+    // search 
+ const search = () =>{
+    const searchbox = document.getElementById('search-item').value.toUpperCae();
+    const storeitems = document.getElementById('book-list')
+    const product = document.querySelectorAll('.featured__card ')
+    const pname = storeitems.getElementsByName('h2')
+
+
+    for(var i =0; i< pname.length; i++){
+
+        let match = product[i].getElementsByTagName('h2')[0];
+
+        if(match){
+            let textvalue = match.textContent || match.innerHTML
+
+            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+                product[i].style.display = "";
+
+            }
+            else{
+                product[i].style.display = "none";
+
+            }
+        }
+    }
+
+ } 
 
 /*=============== ADD SHADOW HEADER ===============*/
 const shadowHeader = () =>{
